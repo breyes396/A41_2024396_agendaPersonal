548ffe4149dab83aee25de1d7ef6cbb4af7dde26
breyes396 <breyes-2024396@kinal.edu.gt>
2025-08-22 16:02:27 -0600

    Primer commit

---
+++ src/main/java/org/binaryminds/agenda_personal/AgendaPersonalApplication.java
@@ -0,0 +1,13 @@
+package org.binaryminds.agenda_personal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaPersonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaPersonalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/binaryminds/agenda_personal/AgendaPersonalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.binaryminds.agenda_personal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaPersonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
